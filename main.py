2f2b6fa8885ce33addb73080e5c2ffbddd780164
Zotov Stanislav <zotovstanislav@mail.ru>
2024-02-10 21:14:27 +0300

    функция пропускающая пустые словари в списке словарей

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ with open(file=os.path.abspath('operations.json'), mode='r', encoding='utf-8') a
 	dict_data = json.load(file)
 
 data = dict_data
+
+def miss_empty_dict():
+	'''
+	если в списке словарей есть пустой словарь, то пропускаем
+	'''
+	data_without_empty = []
+	for i in data:
+		if not i.get('from'):
+			continue
+		data_without_empty.append(i)
+	return data_without_empty
