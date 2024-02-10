c7ccd1844a651d2721077630f8b6af99f595d801
Zotov Stanislav <zotovstanislav@mail.ru>
2024-02-10 21:18:30 +0300

    функция сортирует по дате

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ with open(file=os.path.abspath('operations.json'), mode='r', encoding='utf-8') a
 
 data = dict_data
 
+
 def miss_empty_dict():
 	'''
 	если в списке словарей есть пустой словарь, то пропускаем
@@ -17,9 +18,25 @@ def miss_empty_dict():
 		data_without_empty.append(i)
 	return data_without_empty
 
+
 def get_executed():
 	'''
 	получает список всех EXECUTED операций
 	'''
 	executed_operations = [i for i in miss_empty_dict() if i and i['state'] == 'EXECUTED']
 	return executed_operations
+
+
+def get_data_for_sort(x):
+	'''
+	функция принимает то, по чему в дальнейшем будет сортироваться ф-ия sorted()
+	'''
+	return x['date']
+
+
+def sorted_operations():
+	'''
+	сортирует по дате от настоящего к прошлому
+	'''
+	operations_list = sorted(get_executed(), key=get_data_for_sort, reverse=True)
+	return operations_list
